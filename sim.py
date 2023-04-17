522afdace469bf1ee1514a80d60983687901d7bd
Jon Ingvar Skånøy <jiskaanoy@student.uia.no>
2023-04-17 15:34:49 +0200

    sim komentarer

--- sim.py
+++ sim.py
@@ -10,7 +10,7 @@ import multiprocessing
 
 class Team:
     def __init__(self,name, games_played, goals_for,goals_against,shots_on_goal,shots_against, id):
-        self.t = games_played
+        self.t = games_played 
         self.k = shots_on_goal
         self.binom_a =goals_against
         self.binom_b = shots_against- self.binom_a
@@ -28,24 +28,33 @@ class Team:
 
 def sim_game(team1,team2):
     team1.ma +=1
-    team2.ma +=1
-    team2.match_history.append(team1.id)
+    team2.ma +=1 # hjelpe variabel for å verifisere velykket kjøring av programmet
+    team2.match_history.append(team1.id) # vi lagrer en liste med hvilke lag som har spilt med hvem for å kunne analyserer det i ettertid
     team1.match_history.append(team2.id)
+    # her er poisson prediktiv fordelig negative binom fordeling her sumeres opp 60 1 minutt prosseser
     shots_team1 = sum(nbinom.rvs(team1.k,(team1.poisson_la),size=60))
     shots_team2 = sum(nbinom.rvs(team2.k,(team2.poisson_la),size=60))
+    # her er en bernoulli prosses sin prediktive fordeling beta binomial fordelig, denne kjøres like mange ganger som poisson prossesen ga. 
     goals_team1 = betabinom.rvs(shots_team1,team2.binom_a,team2.binom_b)
     goals_team2 = betabinom.rvs(shots_team2,team1.binom_a,team1.binom_b)
-    if (goals_team1==goals_team2):
+
+    if (goals_team1==goals_team2): # hvis uavgjort
+        # her er poisson prediktiv fordelig negative binom fordeling her sumeres opp 5 1 minutt prosseser, siden man spiller 5 minutter overtid
         shots_team1 = sum(nbinom.rvs(team1.k,(team1.poisson_la),size=5))
         shots_team2 = sum(nbinom.rvs(team1.k,(team2.poisson_la),size=5))
+         # her er en bernoulli prosses sin prediktive fordeling beta binomial fordelig, denne kjøres like mange ganger som poisson prossesen ga. 
         goals_team1 = betabinom.rvs(shots_team1,team2.binom_a,team2.binom_b)
         goals_team2 = betabinom.rvs(shots_team2,team1.binom_a,team1.binom_b)
-        if (goals_team1==goals_team2):
+
+        if (goals_team1==goals_team2):# hvis uavgjort igjen
             while True:
-                goals_team1 = betabinom.rvs(3,team2.binom_a,team2.binom_b)
+                # tre skudd mot mål per lag til en vinner er funnet
+                goals_team1 = betabinom.rvs(3,team2.binom_a,team2.binom_b) 
+
                 goals_team2 = betabinom.rvs(3,team1.binom_a,team1.binom_b)
                 if (goals_team1!=goals_team2):
                     break
+        # setter poeng git utvidet tid
         if (goals_team1<goals_team2):
             team1.points +=1
             team2.points +=2
@@ -56,6 +65,7 @@ def sim_game(team1,team2):
             team2.points +=1
             team1.beaten.append(team2.id)
             return [2,1]
+    # setter poeng gitt avgort i første 60 min
     if (goals_team1<goals_team2):
         team1.points +=0
         team2.points +=2
@@ -69,19 +79,22 @@ def sim_game(team1,team2):
 
 
 def subdivision_simulation(teams):
+    # siden lagene skal forsjellige mengder kamper mot hverandre må vi ha en tilfeldig prosess. 
+    # det ble vurdert slit at det er enkelst at alle spiller mot sine naboer i listen, derfor må listen være i tilfelig rekefølge som shuffle gjør for oss.
     random.shuffle(teams)
     for i in range(0,8):
-        if( i != 7):
-            sim_game(teams[i],teams[i+1])
+        if( i != 7):#denne delingen er grunnet at listen er en liste og ikke en sirkel
+            
+            sim_game(teams[i],teams[i+1])# spill 3 kamper mot neste mann i listen
             sim_game(teams[i],teams[i+1])
             sim_game(teams[i],teams[i+1])
             for team in teams:
                 if (team == teams[i] or team == teams[i+1] or teams[i-1] == team):
                    pass
-                else:
+                else:# spill to kamper mot de andre og når de andre også er her blir det 4 kamper totalt
                     sim_game(team,teams[i])
                     sim_game(team,teams[i])
-        else:
+        else: # samme men tilpasset slik at team[i+1] er erstatted med team[0] 
             sim_game(teams[i],teams[0])
             sim_game(teams[i],teams[0])
             sim_game(teams[i],teams[0])
@@ -94,6 +107,7 @@ def subdivision_simulation(teams):
                
 
 def division_simulation(division):
+    # hvert team skall spille 3 kamper mot hvert team i annen divisjon
     for team in division[0]:
         for i in range(8):
             sim_game(team, division[1][i])
@@ -104,6 +118,7 @@ def division_simulation(division):
 def interconference_simulation(teams):
     div1 = teams[0]
     div2 = teams[1]
+    # hvert team skall spille 2 kamper mot hvert team i annen konforanse
     for subdivision in div1:
         for team in subdivision:
                 for subdivision2 in div2:
@@ -113,7 +128,6 @@ def interconference_simulation(teams):
   
     
 def season_simulation(teams):
-
     for division in teams:
         for subdivision in division:
             subdivision_simulation(subdivision)
@@ -133,10 +147,11 @@ def season_simulation_wrapper(teams,n):
 
 
 def create_teams():
+    # henter data fra pickle filer
     data = pickle.load(open("20-23.p", "rb"))
     names = pickle.load(open("names_20-23.p", "rb"))
     mTeams = []
-
+    # lager liste strukturen
     aTeams = []
     cTeams = []
     pTeams = []
@@ -146,11 +161,11 @@ def create_teams():
     garbagecan = []
     i =0
 
-
+    # går gjennom hvert lag og setter dem i riktig divisjon
     for row in data:
         
         tmp = Team(names[i],row[0],row[1],row[2],row[3],row[4], id=i)
-        
+        # her er inputtet names = navn, # games played = row[0], Goals = row[1], Goals against = row[2], Shots = row[3], Shots against = row[4]
         if(tmp.name in ["Carolina Hurricanes","New Jersey Devils", "New York Rangers","Columbus Blue Jackets", "New York Islanders", "Philadelphia Flyers", "Washington Capitals","Pittsburgh Penguins"]):
             mTeams.append(tmp)
         elif(tmp.name in ["Dallas Stars","Colorado Avalanche","Minnesota Wild", "Winnipeg Jets","Nashville Predators", "St. Louis Blues", "Arizona Coyotes", "Chicago Blackhawks"]):
@@ -162,6 +177,7 @@ def create_teams():
         else:
             garbagecan.append(tmp)
         i +=1
+    # her sender vi lagene til en pickle fil, slik at sansynlighetsfordelinger kan plottes.
     pickle.dump(teams, open("teams_in_div.p", "wb"))
     return teams
 
